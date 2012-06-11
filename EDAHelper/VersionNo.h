89cf672571917e4a902322a13da27d4dc5a76222
chenjun <126186@qq.com>
2012-06-11 19:48:53 +0800

    fix view update & feedback size width

--- EDAHelper/VersionNo.h
+++ EDAHelper/VersionNo.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,1,13,1220
-#define PRODUCTVER     2,1,13,1220
-#define STRFILEVER     "2.1.13.1220\0"
-#define STRPRODUCTVER  "2.1.13.1220\0"
+#define FILEVER        2,1,13,1222
+#define PRODUCTVER     2,1,13,1222
+#define STRFILEVER     "2.1.13.1222\0"
+#define STRPRODUCTVER  "2.1.13.1222\0"
